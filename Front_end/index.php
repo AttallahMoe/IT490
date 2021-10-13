cfe85e7c5e1594c10559c7fadfd5bf2be3a5788a
Mohammed Aref <60104935+ma2233@users.noreply.github.com>
2021-10-13 10:38:57 -0400

    deleted files

--- Front_end/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(__DIR__ . "../...");
-require_once('rabbitMQLib.inc');
-$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-?>
--- Front_end/index_page.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>     Front End</title>
-</head>
-<body>      Hello World</body>
-</html>
