b908071e2ecf13f7b9e3887232e0644ba5605686
AttallahMoe <Attallahmoe@gmail.com>
2021-10-14 16:36:56 -0400

    updates: client functions
    added rabbit client functions to communicate with messaging queue and deleted unnecessary files

--- Back_end/send.php
+++ Back_end/send.php
@@ -1,19 +1,52 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
-use PhpAmqpLib\Conection\AMQPStreamConnection;
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+
+$BROKER_HOST='baboon-01.rmq.cloudamqp.com';
+
+$BROKER_PORT=5672;
+
+$USER='yiowarzc';
+
+$PASS = "vHYTSrG04sVP3jSrmx_ehFS10Hn4aVjy";
+
+$VHOST='yiowarzc';
+
+$EXCHANGE= 'testExchange';
+
+$QUEUE= 'testQueue';
+
+
+
+
+$connection = new AMQPStreamConnection($BROKER_HOST, $BROKER_PORT, $USER, $PASS, $VHOST);
+
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);                                                                           
-                                                                                                                                        
-$msg = new AMQPMessage('Hello Everybody');
+
+
+$channel->queue_declare('hello', false, false, false, false);
+
+
+
+$msg = new AMQPMessage('Hello World!');
+
 $channel->basic_publish($msg, '', 'hello');
 
-echo " [x] Sent 'Hello Everybody'\n";
+
+
+echo " [x] Sent 'Hello World!'\n";
+
+
 
 $channel->close();
+
 $connection->close();
+
 ?>
+     
