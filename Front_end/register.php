e4ad27184083321c5ff53764a1c03cd9ee068f13
Mohammed Aref <60104935+ma2233@users.noreply.github.com>
2021-10-25 21:21:25 -0400

    fixed syntax, working!

--- Front_end/register.php
+++ Front_end/register.php
@@ -3,23 +3,23 @@
 session_start();
 include('RabbitMqFunctions.php');
 
-$user = strtolower($_POST['username']); 
+$user = strtolower($_POST['username']);
 $first = strtolower($_POST['first']);
 $last = strtolower($_POST['last']);
 $webex_link = $_POST['webex'];
-$pass = $_POST['password']
+$pass = $_POST['password'];
 $class_standing = $_POST['standing'];
 
 $response = register($user, $first, $last, $webex_link, $pass, $class_standing);
 
 if($response == true){
-	$_SESSION['username'] = $username;
-	header("location:./home.php");
+        $_SESSION['username'] = $username;
+        header("location:./home.php");
 }
 
-else(){
+else{
     echo("username already taken");
-	header("location:../register.php");
+        header("location:./register.html");
 }
 
 ?>
