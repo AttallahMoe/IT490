27a7fd10e7f3e98efefd49c2988cf7b9e236e3f8
Mohammed Aref <60104935+ma2233@users.noreply.github.com>
2021-12-15 20:56:37 -0500

    created two functions for api

---
+++ Front_end/app_to_playlist.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+global $db;
+
+$db = mysqli_connect("20.55.45.25", "XDiaz241", "canvasdb", "XDSandman2388");
+
+if(!$db)
+{
+    die ('Could not connect:' . mysqli_connect_error());
+}
+
+function add_playlist()
+{
+    
+
+}
+
+        $_SESSION['username'] = $username;
+        header("location:./home.html");
+
+
+else{
+        header("location:./login.html");
+}
+
+?>
