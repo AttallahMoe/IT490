19495c764097c025150d12e1d35ef73114c99881
Mohammed Aref <60104935+ma2233@users.noreply.github.com>
2021-12-16 12:49:01 -0500

    added session

--- Front_end/study_material.php
+++ Front_end/study_material.php
@@ -1,6 +1,7 @@
 <?php
 // Call PHP function from javascript without ajax
-
+session_set_cookie_params(0, "~/RabbitMQ_connect/IT490/RabbitMQ/php_example/html/");
+session_start();
 function add_playlist($link){
     $mydata='Call by function declaration PHP';
     return $mydata;
