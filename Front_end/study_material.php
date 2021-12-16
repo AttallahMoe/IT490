03d67b5259e2d6efb46fed4782450660e3aa3558
Mohammed Aref <60104935+ma2233@users.noreply.github.com>
2021-12-16 12:07:49 -0500

    created study material page

--- Front_end/study_material.php
+++ Front_end/study_material.php
@@ -1,29 +1,37 @@
 <?php
+// Call PHP function from javascript without ajax
 
-session_start();
-global $db;
-
-$db = mysqli_connect("20.55.45.25", "XDiaz241", "canvasdb", "XDSandman2388");
-
-if(!$db)
-{
-    die ('Could not connect:' . mysqli_connect_error());
+function add_playlist($link){
+    $mydata='Call by function declaration PHP';
+    return $mydata;
 }
-
-function display(url)
-{
-    ``````
-
-}
-
-        $_SESSION['username'] = $username;
-        header("location:./home.html");
-
-
-else{
-        header("location:./login.html");
-}
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+                <title> Login Page </title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="csshome.css">
+</head>
 
-display(v=fdjfdk)
+        <body>
+       <div class="navbar">
+        <a href="home.html" class="-button-bar-item">Home</a>
+        <a href="study_material.html" class="-button-bar-item">Study Material</a>
+        <a href="logout.php">Logout<br></a>
+
+       </div>
+
+
+
+       <iframe width="420" height="345" src="https://www.youtube.com/embed/TMubSggUOVE"></iframe>
+        <script>
+        var phpadd= <?php echo add(1,2);?> //call the php add function
+        </script>
+
+       <iframe width="420" height="345" src="https://www.youtube.com/embed/TMubSggUOVE"></iframe>
+       <a href="add_playlist(https://www.youtube.com/embed/TMubSggUOVE)" class="-button-bar-item">add</a><br>
+       </body>
+
+        </html>
